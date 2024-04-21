dc75a53744b0a01fd2cec34be89a61f3a8f8fdb5
Paulo Ricardo <notadead5@gmail.com>
2024-04-21 13:32:34 -0300

    Thumbs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,63 @@ function App() {
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt="" />
-      <h1 className='text-6xl text-white font-black mt-20'>Seu duo está aqui</h1>
+
+      <h1 className='text-transparent bg-nlw-gradient bg-clip-text'>Suas thumbs estão aqui</h1>
+      
+      <div className='grid grid-cols-6 gap-6'>
+
+        <a href="" className='relative rounded-lg overflow-hidden' className='relative rounded-lg overflow-hidden'>
+          <img className='object-cover ' src="/n75pro.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white'>VGN N75 Pro</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div> 
+
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/x86.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white'>X86</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/ak820.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white'>AK820 Pro</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/n75pro.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white'>VGN N75 Pro</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img className='' src="/x86.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white'>VGN N75 Pro</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div> 
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/ak820.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white'>VGN N75 Pro</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div> 
+        </a>
+        
+      </div>
+
+      <div className='bg-[#2A2634] px-8 py-6 mt-8 self-stretch rounded-lg '>
+
+      </div>
     </div>
   )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,8 +7,11 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        backgroundDark: "url('/backgroundark.jpg')"
+        backgroundDark: "url('/backgroundark.jpg')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 8.9) 67.88%)',
       },
+      
     },
   },
   plugins: [],
